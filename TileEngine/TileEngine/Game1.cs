e2dc37fe0427435d724b64dcc94139505812e4bd
Dale-Harrison <dailofernandez@gmail.com>
2012-03-12 23:02:41 +0000

    This is the first commit of some test engine code for a tile map game.

---
+++ TileEngine/TileEngine/Game1.cs
@@ -0,0 +1,264 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TileEngine
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteAnimation vlad;
+
+        TileMap myMap = new TileMap();
+        int squaresAcross = 18;        
+        int squaresDown = 11;
+        int baseOffsetX = -14;
+        int baseOffsetY = -14;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Tile.TileSetTexture = Content.Load<Texture2D>(@"Textures\TileSets\part2_tileset");
+            Camera.ViewWidth = this.graphics.PreferredBackBufferWidth;
+            Camera.ViewHeight = this.graphics.PreferredBackBufferHeight;
+            Camera.WorldWidth = ((myMap.MapWidth - 2) * Tile.TileStepX);
+            Camera.WorldHeight = ((myMap.MapHeight - 2) * Tile.TileStepY);
+            Camera.DisplayOffset = new Vector2(baseOffsetX, baseOffsetY);
+            //Camera.DisplayOffset = new Vector2(0, 0);
+            
+            // TODO: use this.Content to load your game content here
+
+            vlad = new SpriteAnimation(Content.Load<Texture2D>(@"Textures\Characters\T_Vlad_Sword_Walking_48x48"));
+
+            vlad.AddAnimation("WalkEast", 0, 48 * 0, 48, 48, 8, 0.1f);
+            vlad.AddAnimation("WalkNorth", 0, 48 * 1, 48, 48, 8, 0.1f);
+            vlad.AddAnimation("WalkNorthEast", 0, 48 * 2, 48, 48, 8, 0.1f);
+            vlad.AddAnimation("WalkNorthWest", 0, 48 * 3, 48, 48, 8, 0.1f);
+            vlad.AddAnimation("WalkSouth", 0, 48 * 4, 48, 48, 8, 0.1f);
+            vlad.AddAnimation("WalkSouthEast", 0, 48 * 5, 48, 48, 8, 0.1f);
+            vlad.AddAnimation("WalkSouthWest", 0, 48 * 6, 48, 48, 8, 0.1f);
+            vlad.AddAnimation("WalkWest", 0, 48 * 7, 48, 48, 8, 0.1f);
+
+            vlad.AddAnimation("IdleEast", 0, 48 * 0, 48, 48, 1, 0.2f);
+            vlad.AddAnimation("IdleNorth", 0, 48 * 1, 48, 48, 1, 0.2f);
+            vlad.AddAnimation("IdleNorthEast", 0, 48 * 2, 48, 48, 1, 0.2f);
+            vlad.AddAnimation("IdleNorthWest", 0, 48 * 3, 48, 48, 1, 0.2f);
+            vlad.AddAnimation("IdleSouth", 0, 48 * 4, 48, 48, 1, 0.2f);
+            vlad.AddAnimation("IdleSouthEast", 0, 48 * 5, 48, 48, 1, 0.2f);
+            vlad.AddAnimation("IdleSouthWest", 0, 48 * 6, 48, 48, 1, 0.2f);
+            vlad.AddAnimation("IdleWest", 0, 48 * 7, 48, 48, 1, 0.2f);
+
+            vlad.Position = new Vector2(100, 100);
+            vlad.DrawOffset = new Vector2(-24, -38);
+            vlad.DrawOffset = new Vector2(0, 0);
+            vlad.CurrentAnimation = "WalkEast";
+            vlad.IsAnimating = true;
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            Vector2 moveVector = Vector2.Zero;
+            Vector2 moveDir = Vector2.Zero;
+            string animation = "";
+
+            KeyboardState ks = Keyboard.GetState();
+
+            if (ks.IsKeyDown(Keys.NumPad7))
+            {
+                moveDir = new Vector2(-2, -1);
+                animation = "WalkNorthWest";
+                moveVector += new Vector2(-2, -1);
+            }
+
+            if (ks.IsKeyDown(Keys.NumPad8))
+            {
+                moveDir = new Vector2(0, -1);
+                animation = "WalkNorth";
+                moveVector += new Vector2(0, -1);
+            }
+
+            if (ks.IsKeyDown(Keys.NumPad9))
+            {
+                moveDir = new Vector2(2, -1);
+                animation = "WalkNorthEast";
+                moveVector += new Vector2(2, -1);
+            }
+
+            if (ks.IsKeyDown(Keys.NumPad4))
+            {
+                moveDir = new Vector2(-2, 0);
+                animation = "WalkWest";
+                moveVector += new Vector2(-2, 0);
+            }
+
+            if (ks.IsKeyDown(Keys.NumPad6))
+            {
+                moveDir = new Vector2(2, 0);
+                animation = "WalkEast";
+                moveVector += new Vector2(2, 0);
+            }
+
+            if (ks.IsKeyDown(Keys.NumPad1))
+            {
+                moveDir = new Vector2(-2, 1);
+                animation = "WalkSouthWest";
+                moveVector += new Vector2(-2, 1);
+            }
+
+            if (ks.IsKeyDown(Keys.NumPad2))
+            {
+                moveDir = new Vector2(0, 1);
+                animation = "WalkSouth";
+                moveVector += new Vector2(0, 1);
+            }
+
+            if (ks.IsKeyDown(Keys.NumPad3))
+            {
+                moveDir = new Vector2(2, 1);
+                animation = "WalkSouthEast";
+                moveVector += new Vector2(2, 1);
+            }
+
+            if (moveDir.Length() != 0)
+            {
+                vlad.MoveBy((int)moveDir.X, (int)moveDir.Y);
+                if (vlad.CurrentAnimation != animation)
+                    vlad.CurrentAnimation = animation;
+            }
+            else
+            {
+                vlad.CurrentAnimation = "Idle" + vlad.CurrentAnimation.Substring(4);
+            }
+
+            float vladX = MathHelper.Clamp(
+                vlad.Position.X, 0 + vlad.DrawOffset.X, Camera.WorldWidth);
+            float vladY = MathHelper.Clamp(
+                vlad.Position.Y, 0 + vlad.DrawOffset.Y, Camera.WorldHeight);
+
+            Vector2 testPosition = Camera.WorldToScreen(vlad.Position);
+            Camera.Move(testPosition - new Vector2(Camera.ViewWidth /2, Camera.ViewHeight /2));
+            /*
+            if (testPosition.X < 100)
+            {
+                Camera.Move(new Vector2(testPosition.X - 100, 0));
+            }
+
+            if (testPosition.X > (Camera.ViewWidth - 100))
+            {
+                Camera.Move(new Vector2(testPosition.X - (Camera.ViewWidth - 100), 0));
+            }
+
+            if (testPosition.Y < 100)
+            {
+                Camera.Move(new Vector2(0, testPosition.Y - 100));
+            }
+
+            if (testPosition.Y > (Camera.ViewHeight - 100))
+            {
+                Camera.Move(new Vector2(0, testPosition.Y - (Camera.ViewHeight - 100)));
+            }
+            */
+
+            vlad.Position = new Vector2(vladX, vladY);
+            vlad.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            Vector2 firstSquare = new Vector2(Camera.Location.X / Tile.TileWidth, Camera.Location.Y / Tile.TileHeight);
+            int firstX = (int)firstSquare.X;
+            int firstY = (int)firstSquare.Y;
+
+            Vector2 squareOffset = new Vector2(Camera.Location.X % Tile.TileWidth, Camera.Location.Y % Tile.TileHeight);
+            int offsetX = (int)squareOffset.X;
+            int offsetY = (int)squareOffset.Y;
+
+            for (int y = 0; y < squaresDown; y++)
+            {
+                for (int x = 0; x < squaresAcross; x++)
+                {
+                    foreach (int tileID in myMap.Rows[y + firstY].Columns[x + firstX].BaseTiles)
+                    {
+                        spriteBatch.Draw(
+                            Tile.TileSetTexture,
+                            new Rectangle(
+                                (x * Tile.TileWidth) - offsetX, (y * Tile.TileHeight) - offsetY,
+                                Tile.TileWidth, Tile.TileHeight),
+                            Tile.GetSourceRectangle(tileID),
+                            Color.White);
+                    }
+                }
+            }
+            vlad.Draw(spriteBatch, 0, 0);
+
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TileEngine/TileEngine/TileMap.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace TileEngine
+{
+    class MapRow
+    {
+        public List<MapCell> Columns = new List<MapCell>();
+    }
+
+    class TileMap
+    {
+        public List<MapRow> Rows = new List<MapRow>();
+        public int MapWidth = 50;
+        public int MapHeight = 50;
+
+        public TileMap()
+        {
+            for (int y = 0; y < MapHeight; y++)
+            {
+                MapRow thisRow = new MapRow();
+                for (int x = 0; x < MapWidth; x++)
+                {
+                    thisRow.Columns.Add(new MapCell(0));
+                }
+                Rows.Add(thisRow);
+            }
+
+            // Create Sample Map Data
+            Rows[0].Columns[3].TileID = 3;
+            Rows[0].Columns[4].TileID = 3;
+            Rows[0].Columns[5].TileID = 1;
+            Rows[0].Columns[6].TileID = 1;
+            Rows[0].Columns[7].TileID = 1;
+
+            Rows[1].Columns[3].TileID = 3;
+            Rows[1].Columns[4].TileID = 1;
+            Rows[1].Columns[5].TileID = 1;
+            Rows[1].Columns[6].TileID = 1;
+            Rows[1].Columns[7].TileID = 1;
+
+            Rows[2].Columns[2].TileID = 3;
+            Rows[2].Columns[3].TileID = 1;
+            Rows[2].Columns[4].TileID = 1;
+            Rows[2].Columns[5].TileID = 1;
+            Rows[2].Columns[6].TileID = 1;
+            Rows[2].Columns[7].TileID = 1;
+
+            Rows[3].Columns[2].TileID = 3;
+            Rows[3].Columns[3].TileID = 1;
+            Rows[3].Columns[4].TileID = 1;
+            Rows[3].Columns[5].TileID = 2;
+            Rows[3].Columns[6].TileID = 2;
+            Rows[3].Columns[7].TileID = 2;
+
+            Rows[4].Columns[2].TileID = 3;
+            Rows[4].Columns[3].TileID = 1;
+            Rows[4].Columns[4].TileID = 1;
+            Rows[4].Columns[5].TileID = 2;
+            Rows[4].Columns[6].TileID = 2;
+            Rows[4].Columns[7].TileID = 2;
+
+            Rows[5].Columns[2].TileID = 3;
+            Rows[5].Columns[3].TileID = 1;
+            Rows[5].Columns[4].TileID = 1;
+            Rows[5].Columns[5].TileID = 2;
+            Rows[5].Columns[6].TileID = 2;
+            Rows[5].Columns[7].TileID = 2;
+
+            Rows[3].Columns[5].AddBaseTile(30);
+            Rows[4].Columns[5].AddBaseTile(27);
+            Rows[5].Columns[5].AddBaseTile(28);
+
+            Rows[3].Columns[6].AddBaseTile(25);
+            Rows[5].Columns[6].AddBaseTile(24);
+
+            Rows[3].Columns[7].AddBaseTile(31);
+            Rows[4].Columns[7].AddBaseTile(26);
+            Rows[5].Columns[7].AddBaseTile(29);
+
+            Rows[4].Columns[6].AddBaseTile(104);
+
+            Rows[1].Columns[1].Walkable = false;
+            Rows[2].Columns[2].Walkable = false;
+            // End Create Sample Map Data
+        }
+
+
+
+    }
+}
